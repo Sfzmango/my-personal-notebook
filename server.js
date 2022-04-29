e61c9664f3942673a49fbc65d9a7c8fbbb745382
Maung Htike <maunghtike1999@gmail.com>
2022-04-28 18:53:00 -0700

    add notes api page and uuid funct

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const uuid = require('./helpers/uuid');
 
 // set up express and data parsing
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // ~~~~~~~~~ routes ~~~~~~~~~
 // home page
@@ -18,6 +21,17 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
+
+// notes api page
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+})
+
+
+// ~~~~~~~~~ post routes ~~~~~~~~~
+
+
+
 
 //starts the server
 app.listen(PORT, function () {
